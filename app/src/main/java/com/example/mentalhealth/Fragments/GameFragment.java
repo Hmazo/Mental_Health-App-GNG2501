4f71a99792bca86ebb8ba1c47a1766ce5b6db60b
hmazo <hmazo081@uottawa.ca>
2020-12-02 23:22:59 -0500

    hicham

--- app/src/main/java/com/example/mentalhealth/Fragments/GameFragment.java
+++ app/src/main/java/com/example/mentalhealth/Fragments/GameFragment.java
@@ -1,5 +1,7 @@
 package com.example.mentalhealth.Fragments;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.mentalhealth.R;
+import com.mirkoalicastro.birdgame.BirdGame;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,7 +20,7 @@ import com.example.mentalhealth.R;
  * create an instance of this fragment.
  */
 public class GameFragment extends Fragment {
-
+    private ImageView game;
 
 
     public GameFragment() {
@@ -31,6 +35,15 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View view =inflater.inflate(R.layout.fragment_game, container, false);
+        game = view.findViewById(R.id.gameFlappy);
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), BirdGame.class));
+            }
+        });
+
+        return view;
     }
 }
